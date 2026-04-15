79ea59cb2135bd1f13ef808cc73103c8770cb209
STC921 <143191722+STC921@users.noreply.github.com>
2026-04-15 19:39:01 +0900

    Create LongestSubstring.cpp

---
+++ LongestSubstring.cpp
@@ -0,0 +1,48 @@
+// Longest Substring Without Repeating Characters
+
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+// Constraints:
+
+// 0 <= s.length <= 5 * 104
+// s consists of English letters, digits, symbols and spaces.
+
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if(s == "")
+            return 0;
+        int left = 0;
+        vector<int> last(128, -1);
+        int maxLength = 0;
+        for(int right = 0; right < s.size(); right++){
+            if(last[s[right]] >= left){
+                left = last[s[right]] + 1;
+            }
+            last[s[right]] = right;
+            maxLength = max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+};
